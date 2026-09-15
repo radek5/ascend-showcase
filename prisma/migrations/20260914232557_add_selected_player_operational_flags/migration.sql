306de277306901228fbcd6fedd5bbd3c76c61584
radek5 <radekoya@hotmail.com>
2026-09-15 16:37:59 +0000

    Add selected player operational declarations

---
+++ prisma/migrations/20260914232557_add_selected_player_operational_flags/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "ShowcaseSelectedPlayerConfirmation" ADD COLUMN     "hasAccessibilityNeeds" BOOLEAN,
+ADD COLUMN     "hasDietaryRequirements" BOOLEAN,
+ADD COLUMN     "hasMedicalUpdate" BOOLEAN;
